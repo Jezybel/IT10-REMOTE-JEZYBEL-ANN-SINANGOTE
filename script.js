3691576e5fc9b095100f93295ed70983944b0787
Jhon Albert Fernandez <jhonalbertfernandez06@gmail.com>
2025-05-14 10:38:17 +0800

    Added Javascript to handle form submit and assign correct name variable

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("nameForm").addEventListener("submit",function(event){event.preventDefault();}
+
+const input = document.getElementById("name");
+const nameInput = input.ariaValueMax.trim();
+const correctname ="Jez";
